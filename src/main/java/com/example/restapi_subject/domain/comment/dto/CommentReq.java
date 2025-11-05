2f7cbaf8e8cdfbba8c6e3848988f1863f36b959b
Eeeegarden <106652275+Eeeegarden@users.noreply.github.com>
2025-11-05 16:44:34 +0900

    fix: Swagger Req 중복명칭 오류에 따른 @Schema 설정

--- src/main/java/com/example/restapi_subject/domain/comment/dto/CommentReq.java
+++ src/main/java/com/example/restapi_subject/domain/comment/dto/CommentReq.java
@@ -1,15 +1,20 @@
 package com.example.restapi_subject.domain.comment.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 
 
 public record CommentReq() {
+    @Schema(name = "CommentReqCreateDto")
     public record CreateDto(
+            @Schema(description = "댓글 내용", example = "댓글 달고 갑니다~")
             @NotBlank(message = "content_required")
             String content
     ){}
 
+    @Schema(name = "CommentReqUpdateDto")
     public record UpdateDto(
+            @Schema(description = "댓글 수정 내용", example = "수정된 댓글")
             @NotBlank(message = "content_required")
             String content
     ){}
